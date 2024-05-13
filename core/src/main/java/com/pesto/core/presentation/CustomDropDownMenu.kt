a3e09b71a8861e9913c6ba84886cad7b39ca7aa4
nagarajudbng <nagaraj.dbng@gmail.com>
2024-05-13 16:03:48 +0530

    updateding status  added custom dropdown

---
+++ core/src/main/java/com/pesto/core/presentation/CustomDropDownMenu.kt
@@ -0,0 +1,122 @@
+package com.pesto.core.presentation
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.onFocusedBoundsChanged
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.toSize
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.graphics.Color
+import com.pesto.core.domain.states.FieldStatus
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun CustomDropDownMenu(
+    modifier: Modifier,
+//    viewModel: CheckListViewModel(),
+    selected:String,
+    label: String,
+    errorStatus:Error?,
+    list: List<String>,
+    onSelected: (String) -> Unit
+) {
+//    var subCategoryState = viewModel.subCategoryState.value
+    var mExpanded by remember { mutableStateOf(false) }
+    var mSelectedText by remember { mutableStateOf("") }
+
+    var mTextFieldSize by remember { mutableStateOf(Size.Zero) }
+//    val list =
+
+    // Up Icon when expanded and down icon when collapsed
+    val icon = if (mExpanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Box(modifier = modifier) {
+
+
+        // Create an Outlined Text Field
+        // with icon and not expanded
+        OutlinedTextField(
+            value = selected,
+            readOnly = true,
+            enabled = false,
+            onValueChange = {
+//                viewModel.onEvent(CreateMenuEvent.EnteredSubCategory(it))
+            },
+            modifier = modifier
+                .clickable {
+                    mExpanded = true
+                }
+                .onGloballyPositioned { coordinates ->
+                    // This value is used to assign to
+                    // the DropDown the same width
+                    mTextFieldSize = coordinates.size.toSize()
+                },
+            colors = TextFieldDefaults.textFieldColors(
+                disabledTextColor = Color.Black,
+                disabledLabelColor =  Color.Black,
+                disabledTrailingIconColor = Color.Black
+            ),
+            isError = errorStatus == FieldStatus.FieldEmpty,
+//            supportingText = {
+//                if (error == FieldStatus.FieldEmpty) {
+//                    Text(
+//                        modifier = Modifier.fillMaxWidth(),
+//                        text = "Field Selection Required",
+//                        color = MaterialTheme.colorScheme.error
+//                    )
+//                }
+//            },
+            trailingIcon = {
+                Icon(icon, "contentDescription",
+                    Modifier.clickable { mExpanded = !mExpanded })
+            }
+        )
+
+        // Create a drop-down menu with list of cities,
+        // when clicked, set the Text Field text as the city selected
+        DropdownMenu(
+            expanded = mExpanded,
+            onDismissRequest = { mExpanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { mTextFieldSize.width.toDp() })
+        ) {
+            list.forEach { label ->
+                DropdownMenuItem(
+                    text = { Text(text = label) },
+                    onClick = {
+                        onSelected(label)
+                        mSelectedText = label
+                        mExpanded = false
+                    }
+                )
+            }
+        }
+    }
+}
